ce101073de68525cf61a7aacc0607e722409931a
Mohamed-Elfar <st2020048@kfs-hiet.edu.eg>
2026-03-30 12:39:43 +0200

    cleanup the code from unwanted pages and styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,4 @@
 import HeaderOne from "./HeaderOne";
 import HeaderTwo from "./HeaderTwo";
-import HeaderThree from "./HeaderThree";
-import HeaderFour from "./HeaderFour";
-import HeaderFive from "./HeaderFive";
-import HeaderTransparent from "./headerTransparent";
-import HeaderTransparentTwo from "./headerTransparentTwo";
 
-export { HeaderOne, HeaderTwo, HeaderThree, HeaderFour, HeaderFive, HeaderTransparent, HeaderTransparentTwo };
+export { HeaderOne, HeaderTwo };
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,4 @@
 import LayoutOne from "./LayoutOne";
 import LayoutTwo from "./LayoutTwo";
-import LayoutThree from "./LayoutThree";
-import LayoutFour from "./LayoutFour";
-import LayoutFive from "./LayoutFive";
-import LayoutSix from "./LayoutSix";
-import LayoutSeven from "./LayoutSeven";
 
-export { LayoutOne, LayoutTwo, LayoutThree, LayoutFour, LayoutFive, LayoutSix, LayoutSeven };
+export { LayoutOne, LayoutTwo };
