545742024fe5233b0b04b98a6196f762992b46d2
Mohamed-Elfar <st2020048@kfs-hiet.edu.eg>
2026-04-24 18:27:17 +0300

    make the map not show at property details page if there is no embed url

--- src/pages/shop/[slug].js
+++ src/pages/shop/[slug].js
@@ -374,16 +374,20 @@ function ProductDetails({ product, latestBlogs, categories }) {
 
                   <PropertyGallery galleryImages={product.galleryImages} />
 
-                  <h4 className="title-2">Location</h4>
-                  <div className="property-details-google-map mb-60">
-                    <iframe
-                      src={product.mapEmbedUrl || "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27367.521335544432!2d30.974130060919684!3d30.972151866016276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14f7b3b31ed01575%3A0x2ae50b1962e6af90!2z2YLYt9mI2LHYjCDZhdiv2YrZhtipINmC2LfZiNix2Iwg2YXYsdmD2LIg2YLYt9mI2LHYjCDZhdit2KfZgdi42Kkg2KfZhNi62LHYqNmK2Kk!5e0!3m2!1sar!2seg!4v1773696322276!5m2!1sar!2seg"}
-                      width="100%"
-                      height="100%"
-                      frameBorder="0"
-                      allowFullScreen=""
-                    ></iframe>
-                  </div>
+                  {(product.mapEmbedUrl) && (
+                    <>
+                      <h4 className="title-2">Location</h4>
+                      <div className="property-details-google-map mb-60">
+                        <iframe
+                          src={product.mapEmbedUrl || "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27367.521335544432!2d30.974130060919684!3d30.972151866016276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14f7b3b31ed01575%3A0x2ae50b1962e6af90!2z2YLYt9mI2LHYjCDZhdiv2YrZhtipINmC2LfZiNix2Iwg2YXYsdmD2LIg2YLYt9mI2LHYjCDZhdit2KfZgdi42Kkg2KfZhNi62LHYqNmK2Kk!5e0!3m2!1sar!2seg!4v1773696322276!5m2!1sar!2seg"}
+                          width="100%"
+                          height="100%"
+                          frameBorder="0"
+                          allowFullScreen=""
+                        ></iframe>
+                      </div>
+                    </>
+                  )}
 
                   <h4 className="title-2">Floor Plans</h4>
                   {/* <!-- APARTMENTS PLAN AREA START --> */}
